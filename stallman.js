f2cc5976516b8d4e177ce5779bc90dbc1ace2700
Richard Denton <isdampe@gmail.com>
2015-10-22 21:55:22 +1100

    Add signature

--- stallman.js
+++ stallman.js
@@ -40,6 +40,8 @@ var doCommand = function(command, socket) {
 
       socket.write("Data found.\r\nSending your data via email now.\r\n");
 
+      body = body + "\r\n\r\nI am careful in how I use the Internet. I generally do not connect to web sites from my own machine, aside from a few sites I have some special relationship with. I usually fetch web pages from other sites by sending mail to a program (see git://git.gnu.org/womb/hacks.git) that fetches them, much like wget, and then mails them back to me. Then I look at them using a web browser, unless it is easy to see the text in the HTML page directly. I usually try lynx first, then a graphical browser if the page needs it (using konqueror, which won't fetch from other sites in such a situation).\r\n\r\nhttps://stallman.org/stallman-computing.html";
+
       var myMsg = new Email({
         from: "richard@stallman.org",
         to: email,
